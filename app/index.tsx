b2d3d85d0e217375feb5717dacc4954a47c257de
sitimarwa2103 <105841100122@student.unismuh.ac.id>
2025-07-15 19:53:54 +0800

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function TampilanBentuk() {
+  return (
+    <View style={gaya.tataLetakUtama}>
+      <View style={gaya.segitigaVisual} />
+
+      <View style={gaya.bingkaiNama}>
+        <Text style={gaya.teksKonten}>Siti Marwa</Text>
+      </View>
+
+      <View style={gaya.lencanaIdentitas}>
+        <Text style={gaya.teksKonten}>105841100122</Text>
+      </View>
+    </View>
+  );
+}
+
+const gaya = StyleSheet.create({
+  tataLetakUtama: {
+    flex: 1,
+    backgroundColor: '#f1f8e9',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    paddingVertical: 40,
+  },
+  segitigaVisual: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderLeftWidth: 65,
+    borderRightWidth: 65,
+    borderBottomWidth: 100,
+    borderStyle: 'solid',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#6a1b9a',
+  },
+  bingkaiNama: {
+    width: 270,
+    height: 64,
+    backgroundColor: '#0277bd',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 18,
+  },
+  lencanaIdentitas: {
+    width: 270,
+    height: 64,
+    backgroundColor: '#2e7d32',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 18,
+  },
+  teksKonten: {
+    color: '#ffffff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
